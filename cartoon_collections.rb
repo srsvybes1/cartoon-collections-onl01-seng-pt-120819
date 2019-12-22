63f66537d70b6fae3c31387a615b3f55547eff36
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 07:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwares# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captan_planet# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeter_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_chese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
