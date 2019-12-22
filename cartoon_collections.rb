a9221988fb398ea58573544850fa953602b83ffe
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 07:34:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,1 @@
-def roll_call_dwavres(dwarf)
-  dwarf = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarf.each_with_index {|num, dwarf| puts ""}
-end
-
-def summon_captan_planet# code an argument here
-  # Your code here
-end
-
-def long_planeter_calls# code an argument here
-  # Your code here
-end
-
-def find_the_chese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+def r
